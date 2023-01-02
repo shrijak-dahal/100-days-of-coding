e0a5d9af198637b6410b69fd183fc1ede17c6484
shrijak <dahalshrijak@gmail.com>
2023-01-02 23:11:07 +0545

    Day2

---
+++ Day2/SimpleStorage.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber;
+    
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    } 
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+    }
+
+}
